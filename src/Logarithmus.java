27ca703a9e5bdf9170b02be157f47ab7f1918783
Antti Klemm <anttiklemm@googlemail.com>
2020-02-03 13:15:25 +0100

    Initial commit

---
+++ src/Logarithmus.java
@@ -0,0 +1,12 @@
+public class Logarithmus {
+	public static void main(String[] args) {
+		int x = 15;
+
+		int n = 0;
+		for (int z = 1; z < x; n++) {
+			z *= 2;
+		}
+
+		System.out.println(n);
+	}
+}
